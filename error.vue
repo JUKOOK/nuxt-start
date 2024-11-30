117cb518b951da47362bce2c6bd44be8565a1d54
JUKOOK <wnrnrdl@gmail.com>
2024-11-30 17:47:20 +0900

    feat: 에러 페이지 추가

---
+++ error.vue
@@ -0,0 +1,18 @@
+<template>
+  <!-- error 레이아웃 -->
+  <div>
+    <h1>에러 페이지입니다.</h1>
+    <div class="link">
+      <nuxt-link to="/">PC 메인 화면으로 이동</nuxt-link>
+    </div>
+    <div class="link">
+      <nuxt-link to="/mobile">모바일 메인 화면으로 이동</nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  setup(props) {},
+};
+</script>
